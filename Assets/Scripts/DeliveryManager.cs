c3edf03818197ea93505aed34bb64c65deab0c6a
akuchta <akuchta201@gmail.com>
2023-08-04 20:04:48 +0200

    com com com it

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -8,6 +8,8 @@ public class DeliveryManager : MonoBehaviour
 
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
 
     public static DeliveryManager Instance { get; private set; }
     [SerializeField] private RecipeListSO recipeListSO;
@@ -76,12 +78,15 @@ public class DeliveryManager : MonoBehaviour
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }   
             }
         }
         //Brak zamówienia
-        //Gracz dostarczy³ z³e zamówienie 
+        //Gracz dostarczy³ z³e zamówienie
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
+
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList()
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,8 @@ using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour, IKitchenObjectParent  
 {
-  public static Player Instance { get; private set; }  
+    public event EventHandler OnPickedSomething;
+    public static Player Instance { get; private set; }  
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnselectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs {
         public BaseCounter selectedCounter;
@@ -148,6 +149,10 @@ public class Player : MonoBehaviour, IKitchenObjectParent
     public void SetKitchenObject(KitchenObject kitchenObject)
     {
         this.kitchenObject = kitchenObject;
+        if(kitchenObject !=null)
+        {
+            OnPickedSomething?.Invoke(this,EventArgs.Empty);
+        }
     }
     public KitchenObject GetKitchenObject()
     {
