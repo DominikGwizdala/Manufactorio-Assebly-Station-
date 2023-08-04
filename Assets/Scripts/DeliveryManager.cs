951fafa6960c5db0a2307e9322b5076f7f44ea8e
akuchta <akuchta201@gmail.com>
2023-08-04 17:00:02 +0200

    asdsa
    adsa

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+    public static DeliveryManager Instance { get; private set; }
     [SerializeField] private RecipeListSO recipeListSO;
 
     private List<RecipeSO> waitingRecipeSOList;
@@ -13,12 +14,15 @@ public class DeliveryManager : MonoBehaviour
 
     private void Awake()
     {
+        Instance = this;
+
+
         waitingRecipeSOList = new List<RecipeSO>();
     }
     private void Update()
     {
         spawnRecipeTimer -= Time.deltaTime;
-        if (spawnRecipeTimer <=0) 
+        if (spawnRecipeTimer <=0f) 
         {
             spawnRecipeTimer = spawnRecipeTimerMax;
 
@@ -29,6 +33,49 @@ public class DeliveryManager : MonoBehaviour
                 waitingRecipeSOList.Add(waitingRecipeSO);
             }
         }
+    }
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject)
+    {
+        for(int i = 0; i < waitingRecipeSOList.Count; i++)
+        {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if (waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count)
+            {
+                //Ma tak¹ sam¹ liczbê sk³adników
+                bool plateContentsMatchesRecipe = true;
+                foreach(KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList)
+                {
+                    //Pêtla przez wszystkie sk³adniki w Przepisie 
+                    bool ingredientFound = false;
+                    foreach (KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+                    {
+                        //Pêtla przez wszystkie skadniki na talerzu\
+                        if(plateKitchenObjectSO == recipeKitchenObjectSO)
+                        {
+                            //Sk³adniki siê zgadzaj¹
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+                    if (!ingredientFound)
+                    {
+                        //Sk³adnik przepisu nie zosta³ znaleziony na talerzu
+                        plateContentsMatchesRecipe = false;
+                    }
+                }
+                if(plateContentsMatchesRecipe) 
+                {
+                    //W³aœciwe zamówienie
+                    Debug.Log("W³aœciwe zamówienie");
+                    waitingRecipeSOList.RemoveAt(i);
+                    return;
+                }   
+            }
+        }
+        //Brak zamówienia
+        //Gracz dostarczy³ z³e zamówienie 
+        Debug.Log("Niew³aœciwe zamówienie");
     }
 
 }
